700a8cf46c5d6078051711e2429c39fde36e1ffd
Pradeep Chandravadiya <pradeepckn7@gmail.com>
2026-05-02 01:19:45 +0530

    Added output

--- script.py
+++ script.py
@@ -22,15 +22,26 @@ def extract_features(audio, sr=22050):
 
     return mfccs
 
-audio_files = ["gun1.wav", "noise.wav", "gun2.wav", "gun3.wav", "gun4.wav", "noise2.wav", "gun5.wav"]
+audio_files = [
+    "gun1.wav",
+    "noise.wav",
+    "gun2.wav",
+    "gun3.wav",
+    "gun4.wav",
+    "noise2.wav",
+    "gun5.wav"
+]
+
 THRESHOLD = 0.05
 
 for file in audio_files:
+
     print("====================================")
     print(f"Processing: {file}")
 
     try:
         audio, sr = librosa.load(file, sr=22050)
+
     except:
         print(f"Error loading {file}")
         continue
@@ -53,11 +64,21 @@ for file in audio_files:
     print(f"Prediction Score: {prob:.4f}")
 
     if prob > THRESHOLD or "gun" in file:
+
         print("GUNSHOT DETECTED")
 
         filename = f"alert_{int(time.time())}.mp4"
 
-        os.system(f"rpicam-vid -t 5000 --codec libav --width 1280 --height 720 -o {filename}")
+        os.system(
+            f"rpicam-vid -t 5000 --codec libav --width 1280 --height 720 -o {filename}"
+        )
+
+        os.system(
+            f"aws s3 cp {filename} s3://gunshot-detection/"
+        )
+
+        print(f"{filename} uploaded to AWS S3")
+
     else:
         print("No gunshot detected")
 
